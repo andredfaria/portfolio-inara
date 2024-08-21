66c49383c1311c10842aabe3ba18972ccc023503
André Faria <andre.faria@weclever.co>
2024-08-20 22:40:40 -0300

    feat: ajust CSS

--- src/components/Homepage/VideoCarousel/VideoCarousel.js
+++ src/components/Homepage/VideoCarousel/VideoCarousel.js
@@ -6,13 +6,13 @@ import "./VideoCarousel.scss";
 
 const VideoCarousel = ({ videos, title, width = "80%", height = "80%" }) => {
   return (
-    <div className="mt-5 video-carousel-container" style={{ width }}>
+    <div className="mt-3 video-carousel-container" style={{ width }}>
         <img
           className="square-dots dots-img d-none d-lg-block float-right lt"
           src={dotsSquare}
           alt="dots-sq"
         />
-      <h2 className="carousel-title">{title}</h2>
+      <h2 className="about-text carousel-title">{title}</h2>
       <Carousel
         showThumbs={false}
         showStatus={false}
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,25 +2,28 @@ import React from "react";
 import "./_footer.scss";
 import instagram from "../../assets/images/instagram.png";
 import linkedin from "../../assets/images/linkedin.png";
+import whatsapp from "../../assets/images/whatsapp.png";
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <selection className="mt-3 aboutMe container-fluid section-spacing container mt-3 pt-4 pb-4 border-top">
       <div className="container">
-        <div className="row">
-          <div className="col-12 col-lg-6">
+        <div className="row mb-3">
+          <div className="col">
             <ContactForm />
           </div>
-          <div className="col-12 col-lg-6">
-            <SocialIcons />
-          </div>
         </div>
+      </div>
+      <footer className="mt-3 pt-4 pb-4 border-top">
         <div className="row">
           <div className="col-12 text-center">
-            <p className="footer-text">
+            <p className="mb-0">
               Desenvolvido por
-              <a href="https://www.linkedin.com/in/andre-de-faria/"
-                target="_blank" rel="noopener noreferrer"
+              <a
+                href="https://www.linkedin.com/in/andre-de-faria/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-dark"
               >
                 André de Faria &copy;
               </a>{" "}
@@ -28,23 +31,25 @@ const Footer = () => {
             </p>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </selection>
   );
 };
 
 const ContactForm = () => {
   return (
     <div className="contact-form">
-      <h1>Envie um email</h1>
+      <h1 className="h4 m-1">Entre em contato:</h1>
+      <SocialIcons />
       <form
+        className="mt-2"
         target="_blank"
         action="https://formsubmit.co/inaraangra@hotmail.com"
         method="POST"
       >
-        <div className="form-group">
-          <div className="form-row">
-            <div className="col">
+        <div className="mb-3">
+          <div className="row">
+            <div className="col-md-6 m-1">
               <input
                 type="text"
                 name="name"
@@ -53,7 +58,7 @@ const ContactForm = () => {
                 required
               />
             </div>
-            <div className="col">
+            <div className="col-md-6 m-1">
               <input
                 type="email"
                 name="email"
@@ -64,17 +69,17 @@ const ContactForm = () => {
             </div>
           </div>
         </div>
-        <div className="form-group">
+        <div className="mb-3">
           <textarea
             placeholder="Sua mensagem"
             className="form-control"
             name="message"
-            rows="10"
+            rows="5"
             required
           ></textarea>
         </div>
-        <button type="submit" className="btn btn-lg btn-dark btn-block">
-          Enviar 📬
+        <button type="submit" className="btn btn-dark btn-lg btn-block">
+          Enviar um email 📬
         </button>
       </form>
     </div>
@@ -83,26 +88,42 @@ const ContactForm = () => {
 
 const SocialIcons = () => {
   return (
-    <div className="footer-social">
-      <a
-        href="https://www.instagram.com/inaraangra/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-social-icon"
-        aria-label="Instagram"
-      >
-        <img src={instagram} alt="Instagram" />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/inaraangra/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="footer-social-icon"
-        aria-label="LinkedIn"
-      >
-        <img src={linkedin} alt="LinkedIn" />
-      </a>
+    <div className="row">
+      <div className="m-2">
+        <a
+          href="https://www.instagram.com/inaraangra/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+          className="me-3"
+        >
+          <img src={instagram} alt="Instagram" className="img-fluid" style={{ width: '3em', height: '3em' }} />
+        </a>
+        </div>
+        <div className="m-2">
+        <a
+          href="https://www.linkedin.com/in/inaraangra/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+          className="me-3"
+        >
+          <img src={linkedin} alt="LinkedIn" className="img-fluid" style={{ width: '3em', height: '3em' }} />
+        </a>
+      </div>
+       <div className="m-2">
+        <a
+          href="https://wa.me/553598084624/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+          className="me-3"
+        >
+          <img src={whatsapp} alt="LinkedIn" className="img-fluid" style={{ width: '3em', height: '3em' }} />
+        </a>
+      </div>
     </div>
+
   );
 };
 
