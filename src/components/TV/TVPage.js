1b9946c5452cf5fbf04b96d903c578c1dc9e368a
André Faria <andre.faria@weclever.co>
2024-09-07 23:44:42 -0300

    feat: add videos

--- src/components/TV/TVPage.js
+++ src/components/TV/TVPage.js
@@ -1,9 +1,6 @@
 import React, { Fragment, useEffect } from "react";
 import dotsSquare from "../../assets/images/dots-square.png";
-import dots from "../../assets/images/dots.png";
-import line from "../../assets/images/introlines.png";
 import zigzag from "../../assets/images/zigzag.png";
-
 
 import Header from "../common/Header";
 import VideoCarousel from "../Homepage/VideoCarousel/VideoCarousel";
@@ -16,15 +13,41 @@ const TV = (props) => {
     { id: "3", url: "https://www.youtube.com/embed/3JZ_D3ELwOQ" },
   ];
 
+  const prestacao_servico = [
+    { id: "1", url: "https://www.youtube.com/embed/RCLVevPL8hE" },
+    { id: "2", url: "https://www.youtube.com/embed/3chW8DoALhM" },
+    { id: "3", url: "https://www.youtube.com/embed/y_VjWy7ZFLw" },
+    { id: "4", url: "https://www.youtube.com/embed/6LQxd8BbzIU" },
+  ];
+
+  const cultura_e_entreterimento = [
+    { id: "5", url: "https://www.youtube.com/embed/sSrVd0qm2zU" },
+    { id: "6", url: "https://www.youtube.com/embed/Imaw6VNfG1c" },
+    { id: "7", url: "https://www.youtube.com/embed/DDbHcsE52jQ" },
+    { id: "8", url: "https://www.youtube.com/embed/DVMNvDm4aMU" },
+  ];
+
+  const policial = [
+    { id: "13", url: "https://www.youtube.com/embed/bSb5WzBSwTU" },
+    { id: "14", url: "https://www.youtube.com/embed/-Bkwuy_nRBg" },
+  ];
+
+  const esporte = [
+    { id: "19", url: "https://www.youtube.com/embed/gWSd68qov7w" },
+    { id: "20", url: "https://www.youtube.com/embed/KrIQRWXRotM" },
+    { id: "21", url: "https://www.youtube.com/embed/V5npXQRuOak" },
+  ];
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <Fragment>
       <Header />
       <div style={{ color: "#000" }} className="row intro-text-box">
         <div className="col-12">
-          <h1> Portifolio de TV</h1>
+          {/* <h1>Portfólio de TV</h1> */}
         </div>
       </div>
       <img
@@ -32,57 +55,48 @@ const TV = (props) => {
         src={dotsSquare}
         alt="dots-sq"
       />
-      <section
-        id="aboutMe"
-        style={{ backgroundColor: "white" }}
-        className="aboutMe container-fluid section-spacing"
-      >
+
+      {/* Prestação de Serviço */}
+      <section className=" aboutMe container-fluid section-spacing">
         <div className="container">
-          <div className="row">
-            <div className="col-12 about-text-col">
-              <div className="section-heading">
-                <h4 className="about-me-heading">Cultura</h4>
-              </div>
-              <VideoCarousel
-                width="60%"
-                height="80%"
-                videos={videos}
-                title={""}
-              />
-            </div>
+          <div className="section-heading">
+            <h4>Prestação de Serviço</h4>
           </div>
-          {/* <img
-            src={zigzag}
-            style={{ alignItems: "right" }}
-            className="zigzag float-lg-left mt-3 "
-            alt="zig-zag"
-          /> */}
+          <VideoCarousel width="60%" height="80%" videos={prestacao_servico} />
         </div>
       </section>
 
-      <section
-        id="aboutMe"
-        className="aboutMe white container-fluid section-spacing"
-      >
+      {/* Cultura e Entretenimento */}
+      <section className="container-fluid section-spacing">
         <div className="container">
-          <div className="row">
-            <div className="col-12 about-text-col">
-              <div className="section-heading">
-                <h4 className="about-me-heading">Politica</h4>
-              </div>
-              <VideoCarousel
-                width="60%"
-                height="80%"
-                videos={videos}
-                title={""}
-              />
-            </div>
+          <div className="section-heading">
+            <h4>Cultura e Entretenimento</h4>
           </div>
-          <img
-            src={zigzag}
-            className="zigzag position-absolute float-right"
-            alt="zig-zag"
+          <VideoCarousel
+            width="60%"
+            height="80%"
+            videos={cultura_e_entreterimento}
           />
+        </div>
+      </section>
+
+      {/* Policial */}
+      <section className="aboutMe container-fluid section-spacing">
+        <div className="container">
+          <div className="section-heading">
+            <h4>Policial</h4>
+          </div>
+          <VideoCarousel width="60%" height="80%" videos={policial} />
+        </div>
+      </section>
+
+      {/* Esporte */}
+      <section className="container-fluid section-spacing">
+        <div className="container">
+          <div className="section-heading">
+            <h4>Esporte</h4>
+          </div>
+          <VideoCarousel width="60%" height="80%" videos={esporte} />
         </div>
       </section>
 
@@ -90,4 +104,5 @@ const TV = (props) => {
     </Fragment>
   );
 };
+
 export default TV;
